97471759dbc3f84532d2c7d47fab2e891aa31093
EbulfezSadigov <7ag0he8@code.edu.az>
2022-05-26 20:37:25 +0400

    Completed

--- Task1/main.js
+++ Task1/main.js
@@ -1,28 +1,94 @@
-"use strict"
+let addTaskBtn = document.getElementById("add-task");
+let removeTaskBtn = document.getElementById("remove-task");
+let removeSelectedTaskBtn = document.getElementById("remove-selected-task");
+let taskInput = document.querySelector("input.task-title");
+let taskWrapper = document.querySelector(".tasks .list-group");
 
-let Btn = document.querySelector(".add-task");
-let Input = document.querySelector("input.task-title");
-let Wrapper = document.querySelector(".tasks .list-group");
-
-
-Btn.addEventListener("click",function () {
-    if (Input.value.trim()!=0) {
-        let localitems=JSON.parse(localStorage.getItem('localitem'))
-        if (localitems===null) {
-            var tasklist=[];
-        }
-        else{
-            tasklist=localitems;
-        }
-        tasklist.push(Input.value);
-        localStorage.setItem('localitem',JSON.stringify(tasklist))
+if (localStorage.getItem("tasks") == null) {
+  localStorage.setItem("tasks", "[]")
+} else {
+  let tasksFromStorage = JSON.parse(localStorage.getItem("tasks"));
+  if (tasksFromStorage.length > 0) {
+    for (const item of tasksFromStorage) {
+      let newTaskElem =
+        "<li class='list-group-item d-flex justify-content-between align-items-center'>" +
+        item.text +
+        "<span class='badge rounded-pill bg-primary'>" + item.date + "</span>"
+      "</li>";
+      taskWrapper.innerHTML = newTaskElem + taskWrapper.innerHTML;
+      removeTaskBtn.classList.remove("d-none");
+      removeSelectedTaskBtn.classList.remove("d-none");
     }
-    if (Input.value.trim() !== "") {
-        let newElement = "<li class='list-group-item active'>" + Input.value +"</li>"
-        Wrapper.innerHTML = newElement + Wrapper.innerHTML ;
-        Input.value = "";
+  }
+}
+
+taskInput.addEventListener("keypress", function (e) {
+  if (e.keyCode == 13) {
+    saveTask();
+  }
+})
+addTaskBtn.addEventListener("click", saveTask)
+
+function saveTask() {
+  if (taskInput.value.trim() !== "") {
+    let newTaskElem =
+      "<li class='list-group-item d-flex justify-content-between align-items-center'>" +
+      taskInput.value +
+      "<span class='badge rounded-pill bg-primary'>" + getTime() + "</span>"
+    "</li>";
+    taskWrapper.innerHTML = newTaskElem + taskWrapper.innerHTML;
+
+    let task = {
+      text: taskInput.value,
+      date: getTime()
+    }
+
+    let tasksFromStorage = JSON.parse(localStorage.getItem("tasks"));
+    tasksFromStorage.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasksFromStorage));
+
+    taskInput.value = "";
+    removeTaskBtn.classList.remove("d-none");
+    removeSelectedTaskBtn.classList.remove("d-none");
+  } else {
+    alert("You can't add an empty task!");
+  }
+}
+
+
+removeTaskBtn.addEventListener("click", () => {
+  if (window.confirm('Do you really want to delete all tasks?')) {
+    document.querySelectorAll(".list-group-item").forEach(e => {
+      e.remove();
+    })
+    localStorage.setItem("tasks", "[]")
+    removeTaskBtn.classList.add("d-none")
+    removeSelectedTaskBtn.classList.add("d-none")
+  } else {
+    alert("No problem didn't delete any!");
+  }
+})
+
+
+removeSelectedTaskBtn.addEventListener("click", () => {
+  document.querySelectorAll(".list-group-item.active").forEach(e => {
+    e.remove();
+  })
+})
+
+
+function getTime() {
+  let now = new Date();
+  return `${now.getDate() < 10 ? '0' + now.getDate() : now.getDate()}.${now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1}.${now.getFullYear()} / ${now.getHours()}:${now.getMinutes()}`;
+}
+
+document.addEventListener("click", function (e) {
+  if (e.target.getAttribute("class").includes("list-group-item")) {
+    if (!e.target.getAttribute("class").includes("active")) {
+      e.target.classList.add("active")
     }
     else {
-        alert("Empty task!");
+      e.target.classList.remove("active")
     }
+  }
 })
--- Task2/js/basket.js
+++ Task2/js/basket.js
@@ -2,42 +2,61 @@
 
 let tbody = document.querySelector("table tbody");
 let removeAll = document.querySelector(".remove-all");
+let removeSpecs;
 
-if (localStorage.getItem("favourites") === null || JSON.parse(localStorage.getItem("favourites")).length === 0) {
-    let tr = "<tr><td colspan='4' class='text-center text-danger fw-bold'>No items</td></tr>"
-    removeAll.classList.add("d-none")
-    tbody.innerHTML += tr;
-}
-else {
-    let favourites = JSON.parse(localStorage.getItem("favourites"));
-    let total = 0;
-    for (const product of favourites) {
-        let tr = `
+function renderPage() {
+    tbody.innerHTML = "";
+    if (localStorage.getItem("favourites") === null || JSON.parse(localStorage.getItem("favourites")).length === 0) {
+        let tr = "<tr><td colspan='4' class='text-center text-danger fw-bold'>No items</td></tr>"
+        removeAll.classList.add("d-none")
+        tbody.innerHTML += tr;
+    }
+    else {
+        let favourites = JSON.parse(localStorage.getItem("favourites"));
+        let total = 0;
+        for (const product of favourites) {
+            let tr = `
+            <tr>
+                <td><img src='${product.image}'></td>
+                <td>${product.name}</td>
+                <td>${product.desc}</td>
+                <td class='price'>${product.price} * ${product.count}</td>
+                <td><button class="btn btn-danger remove-spec" data-id="${product.id}"><i class="bi bi-trash"></i></button></td>
+            </tr>
+            `
+            tbody.innerHTML += tr;
+
+            let amount = product.price.replace("$", "");
+            amount = parseInt(amount);
+            total += amount * product.count;
+        }
+
+        let subtotalTr = `
         <tr>
-            <td><img src='${product.image}'></td>
-            <td>${product.name}</td>
-            <td>${product.desc}</td>
-            <td class='price'>${product.price} * ${product.count}</td>
+            <td colspan='3' class='text-end'>Subtotal:</td>
+            <td colspan='2' class='price'>$${total}</td>
         </tr>
         `
-        tbody.innerHTML += tr;
+        tbody.innerHTML += subtotalTr;
 
-        let amount = product.price.replace("$", "");
-        amount = parseInt(amount);
-        total += amount * product.count;
     }
-
-    let subtotalTr = `
-    <tr>
-        <td colspan='3' class='text-end'>Subtotal:</td>
-        <td class='price'>$${total}</td>
-    </tr>
-    `
-    tbody.innerHTML += subtotalTr;
-
+    removeSpecs = document.querySelectorAll(".remove-spec");
+    
+    for (const item of removeSpecs) {
+        item.addEventListener("click", function () {
+            let favourites = JSON.parse(localStorage.getItem("favourites"));
+            favourites = favourites.filter(product => product.id != item.getAttribute("data-id"));
+            localStorage.setItem("favourites", JSON.stringify(favourites));
+            renderPage();
+        })
+    }
 }
+
+renderPage();
+
 removeAll.addEventListener("click", function () {
     localStorage.removeItem("favourites");
-    location.reload();
+    renderPage();
 })
+
 
